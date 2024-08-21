f004053fd6c3e5c1c983fd46ac8051cf75a652d9
ambujkumar2022 <108212872+ambujkumar2022@users.noreply.github.com>
2024-08-21 15:02:36 +0200

    Add files via upload
    Properties, Views, wwwroot, appsettings Files are committed

---
+++ EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Cors;
+using System.Net;
+using System.Net.Mail;
+
+namespace Vividly
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var mail = "tutorialseu_dev@outlook.com";
+            var pass = "test123456!";
+
+            var client = new SmtpClient("spmtp_mail.outlook.com", 587)
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(mail, pass)
+            };
+
+            return client.SendMailAsync(
+                  new MailMessage(from: mail,
+                                   to: mail,
+                                   subject,
+                                   message));
+        }
+    }
+}
+ 
